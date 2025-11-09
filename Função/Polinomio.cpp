b427fb684ce18fff768d649ba80d95af61b78d3e
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-08 21:36:49 -0300

    Implementação da classe Polinomio com construtor e métodos de avaliação e derivada.

---
+++ Função/Polinomio.cpp
@@ -0,0 +1,15 @@
+#include "Polinomio.hpp"
+#include <cmath>
+
+Polinomio::Polinomio(double a3, double a2){
+    this->a3 = a3;
+    this->a2 = a2;
+}
+
+double Polinomio::f(double d) const{
+    return a3 * pow(d,3.0) - 9.0 * a2 * d + 3.0;
+}
+
+double Polinomio::df(double d) const{
+    return a3 * 3.0 * pow(d,2.0) - 9.0 * a2; 
+}

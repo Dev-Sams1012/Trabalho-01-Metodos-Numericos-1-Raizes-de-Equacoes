82ed787349d12379fc5d8c5c59d0eeb74b82376d
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-16 10:19:00 -0300

    Refatoração Classe EqCorda: remoção da dependência de Polinomio e implementa métodos.

--- Função/EqCorda.hpp
+++ Função/EqCorda.hpp
@@ -1,21 +1,35 @@
 #ifndef EQ_CORDA_HPP
 #define EQ_CORDA_HPP
 
-
-#include "Polinomio.hpp"
 #include <cmath>
 
-Polinomio::Polinomio(double a3, double a2){
-    this->a3 = a3;
-    this->a2 = a2;
-}
+class EqCorda
+{
 
-double Polinomio::f(double d) const{
-    return a3 * pow(d,3.0) - 9.0 * a2 * d + 3.0;
-}
+private:
+    double a3;
+    double a2;
 
-double Polinomio::df(double d) const{
-    return a3 * 3.0 * pow(d,2.0) - 9.0 * a2; 
-}
+public:
+    EqCorda(double a3, double a2)
+    {
+        this->a3 = a3;
+        this->a2 = a2;
+    }
+
+    double f(double d) const
+    {
+        return a3 * pow(d, 3.0) - 9.0 * a2 * d + 3.0;
+    }
+
+    double df(double d) const
+    {
+        return a3 * 3.0 * pow(d, 2.0) - 9.0 * a2;
+    }
+
+    double df_num(double d, double h) const{
+        return (f(d + h) - f(d)) / h;
+    }
+};
 
 #endif

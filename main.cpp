5ef270a548e212df6554998ab7f836d5dcb1dd10
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-16 10:22:07 -0300

    Corrige caminhos de inclusão e atualiza implementação do método de Newton para usar EqCorda.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
 #include <bits/stdc++.h>
-#include "MetodoNewton.hpp"
-#include "MetodoNewtonFL.hpp"
+#include "Métodos/MetodoNewton.hpp"
+#include "Métodos/MetodoNewtonFL.hpp"
 
 using namespace std;
 
-int main(){
-    Polinomio p (1.0, 1.0);
-    double xk  = metodoNewtonOriginalRec(0.5, p, 0.001, 0,  10000);
+int main()
+{
+    EqCorda p = EqCorda(1.0, 1.0);
+    double xk = metodoNewton(0.5, p, 0.0001, 10000);
+
     cout << "Item A)\n";
     printf("xk = %lf\n", xk);
     printf("F(xk) = %lf\n", p.f(xk));
 
-    xk = metodoNewtonFL(0.5, p, 0.001, 0.05, 10000);
+    xk = metodoNewtonFL(0.5, p, 0.0001, 0.05, 10000);
+
     cout << "Item B)\n";
     printf("xk = %lf\n", xk);
     printf("F(xk) = %lf\n", p.f(xk));
 
+    xk = metodoNewtonDdxNum(0.5, p, 0.0001, 10000);
 
-    
+    cout << "Item C)\n";
+    printf("xk = %lf\n", xk);
+    printf("F(xk) = %lf\n", p.f(xk));
+
     return 0;
 }
